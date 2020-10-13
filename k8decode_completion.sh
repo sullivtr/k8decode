c2b375795e273fca2552d36f38e15f035913f73b
Tyler Sullivan <sullivtr@gmail.com>
2020-10-13 16:02:14 -0700

    fix completion script

--- k8decode_completion.sh
+++ k8decode_completion.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 __k8decode_parse_get()
 {
-local k8decode_output out
-if kubectl_output=$(k8decode --no-headers "$1" 2>/dev/null); then
-	out=($(echo "$(kubectl get secret)" | awk '{print $1}'))
+local out
+	out=($(echo "$(kubectl get secret --all-namespaces)" | awk '{print $2}'))
 	COMPREPLY=( $( compgen -W "${out[*]}" -- "$cur" ) )
-fi
 }
 complete -F __k8decode_parse_get k8decode
